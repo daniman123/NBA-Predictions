63f5a7e76eabf3c3e2209744bdca8d1a7a1b8bc3
daniman123 <>
2024-07-30 23:11:40 +0200

    feat: implemented writing to json tool

--- data_fetching/src/utils/read_write_from_file_tools.rs
+++ data_fetching/src/utils/read_write_from_file_tools.rs
@@ -1,5 +1,5 @@
 use crate::Result;
-use std::{ fs::{ self, File }, io::Read, path::PathBuf };
+use std::{ fs::{ self, File }, io::{ BufWriter, Read, Write }, path::PathBuf };
 
 /// Writes the given contents to a file at the specified path.
 ///
@@ -16,6 +16,56 @@ pub fn write_to_file<P, C>(path: P, contents: C) -> Result<()>
 {
     fs::write(path.into(), contents.as_ref()).map_err(Into::into)
 }
+
+/// Writes the given JSON contents to the specified file path.
+///
+/// # Arguments
+///
+/// * `path` - A value that can be converted into a `PathBuf`, representing the file path where the JSON contents will be written.
+/// * `contents` - A `serde_json::Value` containing the JSON data to be written to the file.
+///
+/// # Returns
+///
+/// This function returns a `Result<()>`:
+/// * `Ok(())` if the JSON contents are successfully written to the file.
+/// * An `Err` variant if an error occurs during the file opening, writing, or flushing operations.
+///
+/// # Errors
+///
+/// This function will return an error in the following situations, but is not limited to just these cases:
+/// * The file does not exist or the user lacks permissions to open it for writing.
+/// * There is insufficient space on the disk to write the JSON contents.
+/// * An I/O error occurs during the writing or flushing process.
+///
+/// # Examples
+///
+/// ```rust
+/// use serde_json::json;
+/// use std::path::PathBuf;
+///
+/// let path = PathBuf::from("output.json");
+/// let contents = json!({ "key": "value" });
+///
+/// match write_json_to_file(path, contents) {
+///     Ok(_) => println!("JSON contents successfully written to file."),
+///     Err(e) => eprintln!("Failed to write JSON to file: {}", e),
+/// }
+/// ```
+///
+/// # Panics
+///
+/// This function will panic if the JSON serialization fails. This is unlikely if the `serde_json::Value` is well-formed.
+pub fn write_json_to_file<P>(path: P, contents: serde_json::Value) -> Result<()>
+where
+    P: Into<PathBuf>
+{
+    let file = File::open(path.into())?;
+    let mut writer = BufWriter::new(file);
+    serde_json::to_writer(&mut writer, &contents).unwrap();
+    writer.flush()?;
+    Ok(())
+}
+
 
 /// Reads all bytes from a file at the given path.
 ///
@@ -48,6 +98,7 @@ mod tests {
     use super::*;
     use std::fs;
     use std::io::Read;
+    use serde_json::json;
 
     #[test]
     fn test_write_to_file() {
@@ -73,5 +124,14 @@ mod tests {
         let path = "../data/raw_response_data/test_output.bin";
         let conversion_results = read_bytes_from_file(path);
         assert!(conversion_results.is_ok())
+    }
+
+    #[test]
+    fn feature() {
+        let path = "../data/json_data/test_output.json";
+        let json_data = json!({"name":"testName","age":123});
+
+        let write_json_result = write_json_to_file(path, json_data);
+        assert!(write_json_result.is_ok())
     }
 }
