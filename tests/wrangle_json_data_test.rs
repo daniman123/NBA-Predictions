dce3eb5af227e4b29334f8de8ebf4cd3ac8b5296
daniman123 <>
2024-07-30 23:12:30 +0200

    test: test cases for json data wrangling

---
+++ tests/wrangle_json_data_test.rs
@@ -0,0 +1,13 @@
+
+#[test]
+pub fn json_data_test() {
+    // let json_data = json_data_result.unwrap();
+
+    // for i in json_data.as_object().unwrap().keys() {
+    //     println!("{:?}", i);
+    // }
+    // //println!("{:?}",);
+
+    // let string_snippet = string_data.split_off(string_data.len() - 850);
+    // println!("{:?}", string_snippet)
+}
