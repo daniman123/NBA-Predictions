a8875f675b641518d881b971c6b17df427f748eb
daniman123 <daniel-saleh@hotmail.com>
2024-08-10 23:53:54 +0200

    feat: joined all condensed team stats dataframe to one dataframe

--- data_preprocessing/src/main.rs
+++ data_preprocessing/src/main.rs
@@ -1,11 +1,14 @@
 pub mod matchups_dataframe;
 mod round_3_processing;
 mod round_4_processing;
+mod round_5_processing;
 
 use round_3_processing::round_3_processing_dataframe;
 use round_4_processing::round_4_processing_dataframe;
+use round_5_processing::round_5_processing_dataframe;
 
 fn main() {
     round_3_processing_dataframe();
     round_4_processing_dataframe();
+    round_5_processing_dataframe();
 }
---
+++ data_preprocessing/src/round_5_processing.rs
@@ -0,0 +1,46 @@
+use polars::prelude::*;
+use read_write::{config_reader::Config, read_df_from_json, write_df_to_json};
+
+pub fn round_5_processing_dataframe() {
+    let config = Config::new();
+    let save_path_round_4_teams_general_advanced = &config.json_data_save_paths_round_4[0];
+    let save_path_round_4_opponent_shooting_general = &config.json_data_save_paths_round_4[1];
+    let save_path_round_4_teams_general_opponent = &config.json_data_save_paths_round_4[2];
+
+    let df_teams_general_advanced = read_df_from_json(save_path_round_4_teams_general_advanced);
+    let df_opponent_shooting_general =
+        read_df_from_json(save_path_round_4_opponent_shooting_general);
+    let df_teams_general_opponent = read_df_from_json(save_path_round_4_teams_general_opponent);
+
+    let mut final_team_stats = df_teams_general_advanced
+        .join(
+            &df_opponent_shooting_general,
+            ["TEAM_ID"],
+            ["TEAM_ID"],
+            JoinArgs::default(),
+        )
+        .unwrap();
+
+    final_team_stats = final_team_stats
+        .join(
+            &df_teams_general_opponent,
+            ["TEAM_ID"],
+            ["TEAM_ID"],
+            JoinArgs::default(),
+        )
+        .unwrap();
+
+    let out = final_team_stats
+        .clone()
+        .lazy()
+        .select([
+            col("*").exclude(["TEAM_ID"]),
+            col("TEAM_ID").cast(DataType::Int32),
+        ])
+        .collect()
+        .unwrap();
+
+    // Store df
+    let save_path_round_5_teams_general_opponent = &config.json_data_save_paths_round_5[0];
+    write_df_to_json(save_path_round_5_teams_general_opponent, out);
+}
